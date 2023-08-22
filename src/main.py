0abd402fa90dd2968b1e73130fb2829d3989b3e8
jyje <jyjeon@outlook.com>
2023-08-23 00:06:04 +0900

    ♻️ apply practical layer structure

--- src/main.py
+++ src/main.py
@@ -1,48 +1,41 @@
+"""Thermal Exporter for Raspberry Pi"""
+import uvicorn
+
 from typer import Typer, Option
 from fastapi import FastAPI
 from typing_extensions import Annotated
-import uvicorn, glob
+
+from thermal.routers.main import router as main_router
 
 typer = Typer(
-    add_completion=False,
-    invoke_without_command=True,
-    rich_markup_mode="rich"
-    )
-api = FastAPI()
+    add_completion         = False,
+    invoke_without_command = True,
+    rich_markup_mode       = "rich"
+)
+
+api = FastAPI(
+
+)
 
 @typer.callback()
 def main(
-    host: str = "0.0.0.0",
-    port: int = 8000,
-    reload: Annotated[bool, Option("-r", "--reload/--no-reload")] = False
-    ):
+    host   : str = "0.0.0.0",
+    port   : int = 8000,
+    reload : Annotated[bool, Option("-r", "--reload")] = False
+):
 
     """
     [blue]Simple API[/blue] to getting [red]thermal informatoin of Raspberry Pi[/red]
-
-    Try [yellow]`GET http://0.0.0.0:8000/thermal`[/yellow] (as default)
     """
-    
+
     uvicorn.run(
-        app="main:api",
-        host=host, 
-        port=port, 
-        reload=reload
+        app   = "main:api",
+        host  = host,
+        port  = port,
+        reload= reload
     )
 
-@api.get("/thermal")
-def thermal():
-    file_list = glob.glob('/sys/class/thermal/thermal_zone*/temp')
-    temp_array = []
-
-    for file_path in file_list:
-        with open(file_path, 'r') as file:
-            temp_raw = file.read().strip()
-            temp = float(temp_raw)/1000
-            temp_array.append(temp)
-            print(f"{file_path}: {temp:.3f} °C")
-
-    return temp_array
+api.include_router(main_router, prefix="", tags=["main"])
 
 if __name__ == "__main__":
     typer()
